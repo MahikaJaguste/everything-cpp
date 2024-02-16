26b16e3df6ffbff74b1679655804a40cf8e12025
Mahika <mahika@Mahikas-MacBook-Pro.local>
2024-02-16 08:26:37 +0530

    quick sort

---
+++ practiceQuestions/sort/quickSort.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArray(vector<int> arr, int size) {
+    for(int i=0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int partitionOneTraverse(vector<int> &arr, int start, int end) {
+    int pivotValue = arr[start];
+    int i = start, j = end, curr = start+1;
+    while(i<j){
+        // if number is smaller, just bring it to i
+        // no need of swapping because we have already been through elements on the left of curr
+        if(arr[curr]<pivotValue) {
+            arr[i] = arr[curr];
+            i++;
+            curr++;
+        }
+        else {
+            // means number of elements to parse finished
+            if(j==curr) {
+                break;
+            }
+            swap(arr[j], arr[curr]);
+            // curr is not incremented as now we need to inspect the swapped number
+            j--;
+        }
+    }
+    arr[i] = pivotValue;
+    return i;
+}
+
+int partition(vector<int> &arr, int start, int end) {
+    int pivotValue = arr[start];
+    int pivotIndex = start;
+    for(int k=start+1;k<=end;k++) {
+        if(arr[k]<=pivotValue) {
+            pivotIndex++;
+        }
+    }
+    swap(arr[start], arr[pivotIndex]);
+
+    int i = start, j = end;
+    while(i<pivotIndex){
+        // if number on the left is large than pivot, swap
+        // and inspect the swapped number
+        if(arr[i]>pivotValue) {
+            swap(arr[i],arr[j]);
+            j--;
+        }
+        else {
+            // move to the next element
+            i++;
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort(vector<int> &arr, int start, int end) {
+
+    // base case
+    if(start >= end) {
+        return;
+    }
+
+    // partition the array
+    int pivot = partition(arr, start, end);
+    // int pivot = partitionOneTraverse(arr, start, end);
+    
+    // one pass complete
+    // now recurse on both partitions
+    quickSort(arr, start, pivot-1);
+    quickSort(arr, pivot+1, end);
+}
+    
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    int x;
+    for(int i=0;i<n;i++) {
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    printArray(arr, n);
+    quickSort(arr, 0, n-1);
+    printArray(arr, n);
+
+}
